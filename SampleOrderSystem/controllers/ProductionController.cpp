834cc973fc5ba69694814101b945b6ea4aca733b
Final-JK <finaljk@gmail.com>
2026-06-12 17:27:26 +0900

    [fix] completeJob 다중 저장 실패 시 조기 반환 및 오류 로그
    [빌드 검증 상태]
    - app-build: PASS
    - unit-test: PASS (16/16)
    
    Signed-off-by: Final-JK <finaljk@gmail.com>

--- SampleOrderSystem/controllers/ProductionController.cpp
+++ SampleOrderSystem/controllers/ProductionController.cpp
@@ -1,6 +1,7 @@
 #include "controllers/ProductionController.h"
 #include "utils/DateTimeHelper.h"
 #include <algorithm>
+#include <iostream>
 
 ProductionController::ProductionController(IRepository<ProductionJob>* productionRepo,
                                             IRepository<Order>*         orderRepo,
@@ -43,8 +44,17 @@ bool ProductionController::completeJob(const std::string& jobId) {
     order.m_status    = OrderStatus::CONFIRMED;
     order.m_updatedAt = now;
 
-    m_productionRepo->save(job);
-    m_sampleRepo->save(sample);
-    m_orderRepo->save(order);
+    if (!m_productionRepo->save(job)) {
+        std::cerr << "[ERROR] completeJob: ProductionJob 저장 실패\n";
+        return false;
+    }
+    if (!m_sampleRepo->save(sample)) {
+        std::cerr << "[ERROR] completeJob: Sample 저장 실패 (job 은 이미 저장됨)\n";
+        return false;
+    }
+    if (!m_orderRepo->save(order)) {
+        std::cerr << "[ERROR] completeJob: Order 저장 실패 (job+sample 은 이미 저장됨)\n";
+        return false;
+    }
     return true;
 }
